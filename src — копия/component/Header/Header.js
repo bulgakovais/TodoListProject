76ef0228943ceaa3d4bcfbc11ab9a7f2f4135547
Irina <irina2358@yandex.ru>
2022-11-24 11:25:12 +0300

    Create components CreateTodo, Todo. Create store todo.v2

--- src — копия/component/Header/Header.js
+++
@@ -1,8 +0,0 @@
-
-
-export function Header() {
-    return (<>
-        <h1>ToDo List</h1>
-    </>
-    )
-}
--- src — копия/store/todo/selector.js
+++
@@ -1,3 +0,0 @@
-export const getTodosFromState = (state) => state.todos
-
-export const getTodoList = (state) => getTodosFromState(state)?.todoList
